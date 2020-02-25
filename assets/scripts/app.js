09d1a97c06410e0f1e2be7e6c7f13ca773044ec2
Bryant Richards <dragonseyeimages@gmail.com>
2020-02-25 09:10:19 -0600

    The logical AND and OR operators

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,6 +30,15 @@ function writeToLog(
 }
 
 function calculateResult(calculationType) {
+  if (
+    calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE'
+  ) {
+    return;
+  }
+  
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   let mathOperator;
@@ -46,6 +55,7 @@ function calculateResult(calculationType) {
     currentResult /= enteredNumber;
     mathOperator = '/';
   }
+
   createAndWriteOutput(mathOperator, initialResult, enteredNumber);
   writeToLog(calculationType, initialResult, enteredNumber, currentResult);
 }
